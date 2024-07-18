1527886572a85d2e69c2e072b37348e5f53bb631
hriq02 <150181760+hriq02@users.noreply.github.com>
2024-07-18 07:39:05 -0300

    Delete Post directory

--- Post/postBash.bat
+++
@@ -1,3 +0,0 @@
-cd /d %~dp0
-curl "http://endereço/rest/fwmodel/rolos" -H "Content-Type:application/json" -d @tstPost.json
-                                                                                 ^nome do arquivo, para facilitar dispare esse comando no local do arquivo
